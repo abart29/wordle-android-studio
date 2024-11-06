a8d9d89e4d2f2f5d4bbd510a2c94705b3eda7c4e
Joe Winter <joewinter333@gmail.com>
2024-11-06 15:33:47 +0000

    added text box

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,7 +50,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WordleApp(){
 
-    var userInput by remember { mutableStateOf(list(5){""})}
+//    var userInput by remember { mutableStateOf(list(5){""})}
     Column(
         modifier = Modifier
         .padding(horizontal = 40.dp),
@@ -59,11 +65,12 @@ fun WordleApp(){
         )
         Row {
             RowOfLetters(
-                answer = userInput,
-                onValueChange = { userInput = it},
+                answer = "",
+                onValueChange = { },
                 modifier = Modifier
             )
         }
+        TextBox()
     }
 }
 
@@ -82,11 +89,39 @@ fun RowOfLetters(
             )
 }
 
+@Composable
+fun TextBox() {
+    var input by remember { mutableStateOf("") }
+    Card(
+       modifier = Modifier.size(width = 100.dp, height = 100.dp),
+        border = BorderStroke(width = 2.dp, color = Color.Blue)
+    ) {
+        TextField(
+            value = input,
+            onValueChange = { input = it },
+            colors = TextFieldDefaults.colors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+            )
+
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TextBoxPreview() {
     WordleTheme {
-        WordleApp()
+        TextBox()
     }
 }
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    WordleTheme {
+//        WordleApp()
+//    }
+//}
