39b970b3d4ed65619f0db458af6e6ac60ccfee25
Joe Winter <joewinter333@gmail.com>
2024-11-06 15:55:18 +0000

    text field automatically changes

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -19,13 +19,16 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -64,13 +67,14 @@ fun WordleApp(){
                 .padding(80.dp)
         )
         Row {
-            RowOfLetters(
-                answer = "",
-                onValueChange = { },
-                modifier = Modifier
-            )
+//            RowOfLetters(
+//                answer = "",
+//                onValueChange = { },
+//                modifier = Modifier
+//            )
+            TextBoxRow()
         }
-        TextBox()
+
     }
 }
 
@@ -80,25 +84,73 @@ fun RowOfLetters(
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    for (i in 1..5)
-            TextField(
-                value = answer,
-                onValueChange = onValueChange,
-                singleLine = true,
-                modifier = Modifier
-            )
+//    for (i in 1..5)
+//            TextField(
+//                value = answer,
+//                onValueChange = onValueChange,
+//                singleLine = true,
+//                modifier = Modifier
+//            )
+//        TextBox()
 }
 
 @Composable
-fun TextBox() {
-    var input by remember { mutableStateOf("") }
+fun TextBoxRow() {
+    val focusManager = LocalFocusManager.current
+    var letter1 by remember { mutableStateOf("") }
+//    val onLetter1Change = { letter1 = it }
+    var letter2 by remember { mutableStateOf("") }
+    var letter3 by remember { mutableStateOf("") }
+    var letter4 by remember { mutableStateOf("") }
+    var letter5 by remember { mutableStateOf("") }
+    LaunchedEffect(key1 = letter1) {
+        if (letter1.isNotEmpty()) {
+            focusManager.moveFocus(
+                focusDirection = FocusDirection.Next
+            )
+        }
+    }
+    LaunchedEffect(key1 = letter2) {
+        if (letter1.isNotEmpty()) {
+            focusManager.moveFocus(
+                focusDirection = FocusDirection.Next
+            )
+        }
+    }
+    LaunchedEffect(key1 = letter3) {
+        if (letter1.isNotEmpty()) {
+            focusManager.moveFocus(
+                focusDirection = FocusDirection.Next
+            )
+        }
+    }
+    LaunchedEffect(key1 = letter4) {
+        if (letter1.isNotEmpty()) {
+            focusManager.moveFocus(
+                focusDirection = FocusDirection.Next
+            )
+        }
+    }
+
+    Row {
+        TextBox(input = letter1, onTextChange = {letter1 = it})
+        TextBox(input = letter2, onTextChange = {letter2 = it})
+        TextBox(input = letter3, onTextChange = {letter3 = it})
+        TextBox(input = letter4, onTextChange = {letter4 = it})
+        TextBox(input = letter5, onTextChange = {letter5 = it})
+    }
+}
+
+@Composable
+fun TextBox(input:String, onTextChange: (String) -> Unit) {
+//    var input by remember { mutableStateOf("") }
     Card(
-       modifier = Modifier.size(width = 100.dp, height = 100.dp),
+       modifier = Modifier.size(width = 50.dp, height = 50.dp),
         border = BorderStroke(width = 2.dp, color = Color.Blue)
     ) {
         TextField(
             value = input,
-            onValueChange = { input = it },
+            onValueChange = onTextChange,
             colors = TextFieldDefaults.colors(
                 unfocusedIndicatorColor = Color.Transparent,
                 focusedIndicatorColor = Color.Transparent,
@@ -109,19 +161,19 @@ fun TextBox() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun TextBoxPreview() {
-    WordleTheme {
-        TextBox()
-    }
-}
-
-
 //@Preview(showBackground = true)
 //@Composable
-//fun GreetingPreview() {
+//fun TextBoxPreview() {
 //    WordleTheme {
-//        WordleApp()
+////        TextBox()
 //    }
 //}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WordleTheme {
+        WordleApp()
+    }
+}
